0f2c523c46dd00af77a54b79f7368f8f018c7511
Todd Halfpenny <todd@mobilecaddy.net>
2018-03-13 11:04:17 +0000

    First drop of new files for Ionic 2+

---
+++ constants.js
@@ -0,0 +1,5 @@
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-03-12 15:46:01 */
+/* git info: "2018-03-08 11:35:36 +0000": 34e66b2d6ddf746b6814f8563e576e511cc89c8f (v2) */
+/* Copyright 2018 MobileCaddy Ltd */
+
+export const START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc";export const START_PAGE_CONTROLLER_VSN="001";export const QUERY_BUFFER_SIZE=50;export const INITIAL_APPCACHE_INFO=[];
---
+++ geoLocation.js
@@ -0,0 +1,5 @@
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-03-12 15:46:01 */
+/* git info: "2018-03-08 11:35:36 +0000": 34e66b2d6ddf746b6814f8563e576e511cc89c8f (v2) */
+/* Copyright 2018 MobileCaddy Ltd */
+
+function getLocation(o,r){var a=[];a.Error="Location information is unavailable.",a.ErrorNumber=2,o(a)}export{getLocation};
