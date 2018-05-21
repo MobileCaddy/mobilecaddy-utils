7892cc17966da5b7503563a8025febdf1e517816
Todd Halfpenny <todd@mobilecaddy.net>
2018-05-21 21:03:41 +0100

    Nightly push

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-04-06 13:52:50 */
-/* git info: "2018-04-06 13:49:20 +0100": 7877a293eee62da2c63a04332bb68746c8312d48 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-21 21:01:28 */
+/* git info: "2018-05-21 20:56:14 +0100": a6c8930a1910fb38f55c08e517b78094c867a7f9 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=50;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-04-06 13:52:50 */
-/* git info: "2018-04-06 13:49:20 +0100": 7877a293eee62da2c63a04332bb68746c8312d48 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-21 21:01:28 */
+/* git info: "2018-05-21 20:56:14 +0100": a6c8930a1910fb38f55c08e517b78094c867a7f9 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
