d7f5227d63b996ab0b76631196dc5479af8a0ce4
Todd Halfpenny <todd@mobilecaddy.net>
2018-05-29 13:10:52 +0100

    v2 updates during testing

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-21 21:01:28 */
-/* git info: "2018-05-21 20:56:14 +0100": a6c8930a1910fb38f55c08e517b78094c867a7f9 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-25 11:48:51 */
+/* git info: "2018-05-24 16:05:38 +0100": ea33860aa98ffe3fa054c62a8407c220e0bcdf5e (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=50;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=1e3,QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE_SL=1e3;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-21 21:01:28 */
-/* git info: "2018-05-21 20:56:14 +0100": a6c8930a1910fb38f55c08e517b78094c867a7f9 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-25 11:48:51 */
+/* git info: "2018-05-24 16:05:38 +0100": ea33860aa98ffe3fa054c62a8407c220e0bcdf5e (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
