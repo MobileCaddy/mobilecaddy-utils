6834fe3f240a71eec747b60baf1c797fed633cf5
Todd Halfpenny <todd@mobilecaddy.net>
2018-06-07 16:11:17 +0100

    Various, including adding missing let / var, and USE_FORCETK to devUtils

--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-05-25 11:48:51 */
-/* git info: "2018-05-24 16:05:38 +0100": ea33860aa98ffe3fa054c62a8407c220e0bcdf5e (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-06-07 16:10:32 */
+/* git info: "2018-06-07 16:10:18 +0100": d15ade09940faa46c0f7e2cd96cdd4e5bc098409 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
