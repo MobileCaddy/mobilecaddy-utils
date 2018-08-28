21a58b5b890ea6d70ac90c0abe996024bce76c43
Todd Halfpenny <todd@mobilecaddy.net>
2018-08-28 14:15:23 +0100

    Lots of updates brought in

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-06-28 16:59:28 */
-/* git info: "2018-06-28 16:59:04 +0100": 03567979314fa078891c57fd2e18034d16128015 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-08-28 14:07:42 */
+/* git info: "2018-08-02 15:26:12 +0100": 215f3036c36884a4ede49961e39cd99fa994b892 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=1e3,QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE_SL=1e3;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-06-28 16:59:28 */
-/* git info: "2018-06-28 16:59:04 +0100": 03567979314fa078891c57fd2e18034d16128015 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-08-28 14:07:42 */
+/* git info: "2018-08-02 15:26:12 +0100": 215f3036c36884a4ede49961e39cd99fa994b892 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
