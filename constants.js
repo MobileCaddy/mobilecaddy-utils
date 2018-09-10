3eceb0629e80fd431bd49ae91e3b1d3b406d7d6d
Todd Halfpenny <todd@mobilecaddy.net>
2018-09-10 13:43:20 +0100

    Support proxy IDs in SOQL 'IN' syntax

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-07 11:03:38 */
-/* git info: "2018-09-07 10:52:31 +0100": 86feeafc0686d53e19dfbf9077e67117ccac8edc (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-10 13:42:32 */
+/* git info: "2018-09-10 13:41:13 +0100": b8e025ed252ff0d8eecc501c7ccdff0408e732d7 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=1e3,QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE_SL=1e3;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-07 11:03:38 */
-/* git info: "2018-09-07 10:52:31 +0100": 86feeafc0686d53e19dfbf9077e67117ccac8edc (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-10 13:42:32 */
+/* git info: "2018-09-10 13:41:13 +0100": b8e025ed252ff0d8eecc501c7ccdff0408e732d7 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
