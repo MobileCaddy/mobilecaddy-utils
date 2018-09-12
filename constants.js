dc1134a7179e29635a99bd870baa736ed0a73a5e
Todd Halfpenny <todd@mobilecaddy.net>
2018-09-12 10:16:09 +0100

    Should fix issue with proxyIDs in smartSql, when running on device

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-10 13:42:32 */
-/* git info: "2018-09-10 13:41:13 +0100": b8e025ed252ff0d8eecc501c7ccdff0408e732d7 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-12 09:55:03 */
+/* git info: "2018-09-11 16:36:47 +0100": ca93ca8621ceb8afaf749cbab3093ac5b3712997 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});var START_PAGE_CONTROLLER=exports.START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc",START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER_VSN="001",QUERY_BUFFER_SIZE=exports.QUERY_BUFFER_SIZE=1e3,QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE_SL=1e3;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-10 13:42:32 */
-/* git info: "2018-09-10 13:41:13 +0100": b8e025ed252ff0d8eecc501c7ccdff0408e732d7 (v2) */
+/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-12 09:55:03 */
+/* git info: "2018-09-11 16:36:47 +0100": ca93ca8621ceb8afaf749cbab3093ac5b3712997 (v2) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
