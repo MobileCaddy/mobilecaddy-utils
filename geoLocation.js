7acfc63dd610ed0467a6717476c76d251ae4f29d
Todd Halfpenny <todd@mobilecaddy.net>
2018-11-23 16:03:25 +0000

    First drop of fileUtils

--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.0 - Bundle Time: 2018-09-12 09:55:03 */
-/* git info: "2018-09-11 16:36:47 +0100": ca93ca8621ceb8afaf749cbab3093ac5b3712997 (v2) */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-11-23 15:59:59 */
+/* git info: "2018-10-31 14:17:44 +0000": a72a874417833940c00a39b9dbd87e3b3b027dd0 (msd-628/p2m-fileUtils) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
