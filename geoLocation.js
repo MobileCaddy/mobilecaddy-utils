c4af943a725748982744204ddfe608a6472bf27c
Todd Halfpenny <todd@mobilecaddy.net>
2018-11-26 16:21:17 +0000

    Protected CodeFlow against unknown cordova.file

--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-11-23 15:59:59 */
-/* git info: "2018-10-31 14:17:44 +0000": a72a874417833940c00a39b9dbd87e3b3b027dd0 (msd-628/p2m-fileUtils) */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-11-26 16:18:13 */
+/* git info: "2018-11-23 16:02:33 +0000": f458f3fe3659eef32ccde089f82b18d5376b87d6 (msd-628/p2m-fileUtils) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
