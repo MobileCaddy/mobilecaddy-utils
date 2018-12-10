5536801568785e80f057a2b49f9c90b61daa8d76
Todd Halfpenny <todd@mobilecaddy.net>
2018-12-10 09:09:57 +0000

    x-platform files support with fileUtils.readAsDataURL()

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-11-29 10:57:03 */
-/* git info: "2018-11-23 16:02:33 +0000": f458f3fe3659eef32ccde089f82b18d5376b87d6 (msd-628/p2m-fileUtils) */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-10 09:08:57 */
+/* git info: "2018-12-07 16:26:19 +0000": d8ff591bd903dccc772626a1bacc1be7a8af2829 (msd-628/p2m-fileUtils) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.STANDING_DATA_OBJECT=exports.DYNAMIC_DATA_OBJECT=exports.QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE=exports.START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER=void 0;var START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc";exports.START_PAGE_CONTROLLER=START_PAGE_CONTROLLER;var START_PAGE_CONTROLLER_VSN="001";exports.START_PAGE_CONTROLLER_VSN=START_PAGE_CONTROLLER_VSN;var QUERY_BUFFER_SIZE=1e3;exports.QUERY_BUFFER_SIZE=QUERY_BUFFER_SIZE;var QUERY_BUFFER_SIZE_SL=1e3;exports.QUERY_BUFFER_SIZE_SL=QUERY_BUFFER_SIZE_SL;var DYNAMIC_DATA_OBJECT=void 0;exports.DYNAMIC_DATA_OBJECT=DYNAMIC_DATA_OBJECT;var STANDING_DATA_OBJECT=void 0;exports.STANDING_DATA_OBJECT=STANDING_DATA_OBJECT;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-11-29 10:57:03 */
-/* git info: "2018-11-23 16:02:33 +0000": f458f3fe3659eef32ccde089f82b18d5376b87d6 (msd-628/p2m-fileUtils) */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-10 09:08:57 */
+/* git info: "2018-12-07 16:26:19 +0000": d8ff591bd903dccc772626a1bacc1be7a8af2829 (msd-628/p2m-fileUtils) */
 /* Copyright 2018 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
