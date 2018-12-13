10098f068070d10c91a1d74237ef6c8b9a9882f7
Todd Halfpenny <todd@mobilecaddy.net>
2018-12-13 15:45:05 +0000

    Add CodeFlow handling to load and store to local filesystem rather than localStorage

--- logger.js
+++ logger.js
@@ -1,4 +1,4 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-10 09:08:57 */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-13 11:20:24 */
 /* git info: "2018-12-07 16:26:19 +0000": d8ff591bd903dccc772626a1bacc1be7a8af2829 (msd-628/p2m-fileUtils) */
 /* Copyright 2018 MobileCaddy Ltd */
 
