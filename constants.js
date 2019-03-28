6a22673896f5fc6190b10a0a9bde3470790a95e6
Todd Halfpenny <todd@mobilecaddy.net>
2019-03-28 15:00:12 +0000

    Support for platform config

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-13 11:20:24 */
-/* git info: "2018-12-07 16:26:19 +0000": d8ff591bd903dccc772626a1bacc1be7a8af2829 (msd-628/p2m-fileUtils) */
-/* Copyright 2018 MobileCaddy Ltd */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2019-03-28 14:56:38 */
+/* git info: "2019-03-28 14:56:20 +0000": 5b442bdf4f3a9846e8098c3171e7efb248af71ad (v2) */
+/* Copyright 2019 MobileCaddy Ltd */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.STANDING_DATA_OBJECT=exports.DYNAMIC_DATA_OBJECT=exports.QUERY_BUFFER_SIZE_SL=exports.QUERY_BUFFER_SIZE=exports.START_PAGE_CONTROLLER_VSN=exports.START_PAGE_CONTROLLER=void 0;var START_PAGE_CONTROLLER="MobileCaddyStartPageController001_mc";exports.START_PAGE_CONTROLLER=START_PAGE_CONTROLLER;var START_PAGE_CONTROLLER_VSN="001";exports.START_PAGE_CONTROLLER_VSN=START_PAGE_CONTROLLER_VSN;var QUERY_BUFFER_SIZE=1e3;exports.QUERY_BUFFER_SIZE=QUERY_BUFFER_SIZE;var QUERY_BUFFER_SIZE_SL=1e3;exports.QUERY_BUFFER_SIZE_SL=QUERY_BUFFER_SIZE_SL;var DYNAMIC_DATA_OBJECT=void 0;exports.DYNAMIC_DATA_OBJECT=DYNAMIC_DATA_OBJECT;var STANDING_DATA_OBJECT=void 0;exports.STANDING_DATA_OBJECT=STANDING_DATA_OBJECT;
--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2018-12-13 11:20:24 */
-/* git info: "2018-12-07 16:26:19 +0000": d8ff591bd903dccc772626a1bacc1be7a8af2829 (msd-628/p2m-fileUtils) */
-/* Copyright 2018 MobileCaddy Ltd */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2019-03-28 14:56:38 */
+/* git info: "2019-03-28 14:56:20 +0000": 5b442bdf4f3a9846e8098c3171e7efb248af71ad (v2) */
+/* Copyright 2019 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
