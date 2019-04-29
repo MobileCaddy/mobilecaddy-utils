3397c1e1771e6cb6b1a848a505043c0a0911d227
Todd Halfpenny <todd@mobilecaddy.net>
2019-04-29 13:25:30 +0100

    Platform app config.

--- geoLocation.js
+++ geoLocation.js
@@ -1,5 +1,5 @@
-/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2019-03-28 14:56:38 */
-/* git info: "2019-03-28 14:56:20 +0000": 5b442bdf4f3a9846e8098c3171e7efb248af71ad (v2) */
+/* mobilecaddy-utils - v2.0.1 - Bundle Time: 2019-04-29 13:24:12 */
+/* git info: "2019-04-29 13:24:02 +0100": b1f59c7a23c5fc1bd753e154fd2b9713b9128ae8 (v2) */
 /* Copyright 2019 MobileCaddy Ltd */
 
 "use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
