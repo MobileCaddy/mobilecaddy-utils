3bc2124edb794f57f97980edd245c46b010afcfb
Todd Halfpenny <todd@toddhalfpenny.com>
2022-03-29 10:27:17 +0100

    Updates for v3.0.0

---
+++ geoLocation.js
@@ -0,0 +1,5 @@
+/* mobilecaddy-utils - v3.0.0 - Bundle Time: 2022-03-29 10:17:01 */
+/* git info: "2022-02-22 13:12:22 +0000": 739123844363ba576488138e983c8ab4f2016871 (v3) */
+/* Copyright 2022 MobileCaddy Ltd */
+
+"use strict";function getLocation(o,e){var t=[];t.Error="Location information is unavailable.",t.ErrorNumber=2,o(t)}Object.defineProperty(exports,"__esModule",{value:!0}),exports.getLocation=getLocation;
